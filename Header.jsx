ffb9891d1d31fc77a7e275e90a6c52bb26b1d071
HumblePlebeian <71592155+HumblePlebeian@users.noreply.github.com>
2021-06-07 05:44:56 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header(){
+  return(
+    <header>
+      <h1>Shape AI: Committed to the People, Committed to the Future.</h1>
+    </header>
+  );
+}
---
+++ Note.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Note(){
+  return(
+    <div className = "note">
+      <h1>JavaScript and ReactJS</h1>
+      <p>The 7-day workshop was meticulously tailored to provide the right kickstart to the attendees in the field of front-end web development. The beginner bootcamp felicitously covered the fundamentals of JavaScript and ReactJS framework, while helping the participants to understand the dynamism that can integrated into a web application through the introduction of vital concepts like VirtualDOM. </p>
+    </div>
+  );
+}
